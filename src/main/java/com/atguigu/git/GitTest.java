ce786a8e5cc19ec50ac7ce9c69b19c42d43ad7c4
huedu <huedu@qq.com>
2022-08-15 11:07:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Date 2022/8/15 上午 11:03
+ **/
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
